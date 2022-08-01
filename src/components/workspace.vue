737a8da38dc2f074d8a8638161e50166764e2741
noosheen1995 <noosheen.sh1995@yahoo.com>
2022-08-01 12:38:41 +0430

    some change is addde to workspace

--- src/components/workspace.vue
+++ src/components/workspace.vue
@@ -2,15 +2,49 @@
   <v-card>
     <v-card>
       <v-layout>
-        <v-card color="#002a42">
-          <v-list-item class="img">
-            <v-img class="img" src="../assets/img/20.png"></v-img>
+        <v-card color="#002a42" height="100vh" class="sidebar">
+          <v-list-item class="listitem pt-10">
+            <v-img class="" src="../assets/img/20.png"></v-img>
           </v-list-item>
-          <v-list-item>
+          <v-list-item class="listitem2 pt-15 pb-8">
             <v-img src="../assets/img/21.png"></v-img>
           </v-list-item>
-          <v-list-item v-for="item in items" :key="item">
+          <v-list-item
+            class="listitem2 pt-15"
+            v-for="item in items"
+            :key="item"
+          >
             <v-icon class="item">{{ item.icon }}</v-icon>
+          </v-list-item>
+          <v-list-item>
+          <v-card>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant.sync="mini"
+      permanent
+    >
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-title>John Leider</v-list-item-title>
+
+        <v-btn
+          icon
+          @click.stop="mini = !mini"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+       
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
           </v-list-item>
         </v-card>
 
@@ -33,6 +67,13 @@ export default {
         { icon: "mdi-lightning-bolt-outline" },
         { icon: "mdi-message-question-outline" },
       ],
+       drawer: true,
+        hh: [
+          { title: 'Home', icon: 'mdi-home-city' },
+          { title: 'My Account', icon: 'mdi-account' },
+          { title: 'Users', icon: 'mdi-account-group-outline' },
+        ],
+        mini: true,
     };
   },
 };
@@ -42,14 +83,31 @@ export default {
 @mixin colorpart() {
   color: rgb(101, 114, 131);
 }
+@mixin li($h, $w) {
+  max-height: $h;
+  max-width: $w;
+}
+
 .item {
   @include colorpart();
 }
 .listitem {
-  max-height: 50px;
+  @include li(50px, 70px);
+}
+.listitem2 {
+  @include li(40px, 90px);
+}
+.listitem2:nth-child(6) {
+  margin-top: 5rem;
 }
 .yousign {
   background-color: aqua;
   color: rgb(101, 114, 131);
+}
+.sidebar {
+  max-width: 4rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
